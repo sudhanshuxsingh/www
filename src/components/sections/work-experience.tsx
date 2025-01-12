c0f62e4eb9699bfef887959425eb2643e6077ae2
s2-sengar <sudhanshu.iem2k18@gmail.com>
2025-01-12 16:55:36 +0530

    design: update line width

--- src/components/sections/work-experience.tsx
+++ src/components/sections/work-experience.tsx
@@ -6,10 +6,10 @@ export default function WorkExperience() {
                 <p className="font-medium">Tata Consultancy Services</p>
             </div>
             <div className="pl-[24px] relative pb-4 text-muted-foreground">
-                <div className="absolute w-0.5 bg-muted-foreground h-[44px] left-[3px]" />
-                <div className="absolute w-0.5 bg-muted-foreground h-[calc(100%-8px)] top-4 left-[3px]" />
+                <div className="absolute w-[1.5px] bg-muted-foreground h-[44px] left-[3px]" />
+                <div className="absolute w-[1.5px] bg-muted-foreground h-[calc(100%-8px)] top-4 left-[3px]" />
                 <div className="relative pt-4 text-sm">
-                    <div className="w-4 h-3 border-l-[3px] border-b-[3px] rounded-bl-lg absolute -left-[21px] border-muted-foreground" />
+                    <div className="w-4 h-3 border-l-[1.6px] border-b-[1.6px] rounded-bl-lg absolute -left-[21px] border-muted-foreground" />
                     <p className="font-medium">SDE | Innovator Cadre</p>
                     <p className="text-xs">
                         <>
@@ -24,7 +24,7 @@ export default function WorkExperience() {
             </div>
             <div className="pl-[24px] relative pb-4 text-muted-foreground">
                 <div className="relative pt-1 text-sm">
-                    <div className="w-4 h-3 border-l-[3px] border-b-[3px] rounded-bl-lg absolute -left-[21px] border-muted-foreground" />
+                    <div className="w-4 h-3 border-l-[1.6px] border-b-[1.6px] rounded-bl-lg absolute -left-[21px] border-muted-foreground" />
                     <p className="font-medium">SDE | Digital Cadre</p>
                     <p className="text-xs">
                         <>

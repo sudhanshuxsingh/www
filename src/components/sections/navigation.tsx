6439d0657f5904abf2e5de1a850aa6d3a94e26bd
s2-sengar <sudhanshu.iem2k18@gmail.com>
2025-01-12 09:53:57 +0530

    feat: create navigation

---
+++ src/components/sections/navigation.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+    IconBrandGithub,
+    IconBrandX,
+    IconExchange,
+    IconHome,
+    IconNewSection,
+    IconTerminal2,
+} from "@tabler/icons-react";
+
+export function Navigation() {
+    const links = [
+        {
+            title: "Home",
+            icon: (
+                <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+
+        {
+            title: "Products",
+            icon: (
+                <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+        {
+            title: "Components",
+            icon: (
+                <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+        {
+            title: "Aceternity UI",
+            icon: (
+                <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+        {
+            title: "Changelog",
+            icon: (
+                <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+
+        {
+            title: "Twitter",
+            icon: (
+                <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+        {
+            title: "GitHub",
+            icon: (
+                <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+            ),
+            href: "#",
+        },
+    ];
+    return (
+        <div className="flex items-center justify-center w-full fixed bottom-4">
+            <FloatingDock
+                items={links}
+            />
+        </div>
+    );
+}

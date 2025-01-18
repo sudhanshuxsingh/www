0f3fe047244d8f3286ea4862e718d03c824f7031
s2-sengar <sudhanshu.iem2k18@gmail.com>
2025-01-18 21:26:54 +0530

    feat: add command menu for navigation

---
+++ src/components/ui/visually-hidden.tsx
@@ -0,0 +1,12 @@
+import * as Hidden from "@radix-ui/react-visually-hidden";
+import { ReactNode } from "react";
+
+export default function VisuallyHidden({ children }: {
+    children: ReactNode
+}) {
+    return (
+        <Hidden.Root>
+            {children}
+        </Hidden.Root>
+    )
+}
